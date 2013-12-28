1aeaa9d30a47122027f84d13c4332a018e04609e
Jim Friend <jimfriend15@gmail.com>
2013-12-27 18:27:08 -0800

    Fix for the Login page where you would receive an error if there wasn't a 'status' querystring variable present

--- login.php
+++ login.php
@@ -128,12 +128,12 @@ setcookie("userPieUser", $loggedInUser->remember_me_sessid, time()+parseLength($
         } }
         ?> 
         
-        <?php if(($_GET['status']) == "success") 
-        {
+        <?php 
         
-        echo "<p>Your account was created successfully. Please login.</p>";
-        
-    	}
+        if( !empty( $_GET['status'] ) && ( $_GET['status'] ) == "success" ) {
+	        echo "<p>Your account was created successfully. Please login.</p>";
+    	} 
+    	
     	?>
         
         
